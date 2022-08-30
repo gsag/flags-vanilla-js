f36b0a3cb11adfad4d7f4ee4752f0a9b727212ae
Guilherme Alves <gsag.dh@gmail.com>
2022-08-29 22:16:02 -0300

    Adding basic javascript template logic

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 (async () => {
-  const appDiv = document.getElementById('app');
-  appDiv.innerHTML = `<h1>JS Starter</h1>`;
+  const setContainerInnerHtml = (str) => document.querySelector(".country-list-container").innerHTML = str;
+  setContainerInnerHtml("Loading...");
+
+  const countryRawData = await (await fetch("https://restcountries.com/v3.1/all")).json();
+  const countryMappedData = countryRawData.map(country => ({
+    name: country.name,
+    region: country.region,
+    subregion: country.subregion,
+    capital: country.capital,
+    flags: country.flags 
+  }))
+
+  const renderTemplate = (countryList) => {
+    return `
+    <h1>Country List</h1>
+    <ul class="country-list">
+      ${countryList.map(country => `
+        <li class="country-list__item">
+          <img src="${country.flags.png}"/>
+          <span class="${country.region}">${country.name.common}</span>
+        </li>
+      `).join("")}
+    </ul>`
+  }
+  
+  setContainerInnerHtml(renderTemplate(countryMappedData));
 })();
