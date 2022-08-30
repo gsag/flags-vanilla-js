2aa4e977a221cf64a0baa6f0fb9b49695269c349
Guilherme Alves <gsag.dh@gmail.com>
2022-08-30 00:25:18 -0300

    Styling and css animation

--- index.js
+++ index.js
@@ -10,15 +10,25 @@
     capital: country.capital,
     flags: country.flags 
   }))
-
+console.log(countryMappedData)
   const renderTemplate = (countryList) => {
     return `
     <h1>Country List</h1>
     <ul class="country-list">
       ${countryList.map(country => `
-        <li class="country-list__item">
-          <img src="${country.flags.png}"/>
-          <span class="${country.region}">${country.name.common}</span>
+        <li class="country-list__card ${country.region}">
+          <div class="country-list__flag">
+            <div class="country-list__flag--front">
+              <img src="${country.flags.svg}"/>
+            </div>
+            <div class="country-list__flag--flipped">
+              <span>${country.name.common}</span>
+              <span>${country.name.official}</span>
+              <span>${country.capital}</span>
+              <span>${country.region}</span>
+              <span>${country.subregion}</span>
+            </div>
+          </div>
         </li>
       `).join("")}
     </ul>`
